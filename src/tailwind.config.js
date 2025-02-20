ac5189bd9e4bbab6f0d2ce1b8e241175856f0707
Paul Jhon Magbanua <magbanuatx56@gmail.com>
2025-02-21 07:35:06 +0800

    made changes

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html", "./src/**/*.{html,js,jsx,ts,tsx}"], 
+    theme: {
+      extend: {
+        colors: {
+          purple: '#756FEA' 
+        }
+      }
+    },
+    plugins: []
+  }
+  
